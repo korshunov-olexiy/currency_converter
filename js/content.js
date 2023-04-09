809c27fd558323e6deccb02bff0659d243570038
korshunov-olexiy <korshunov.olexiy@gmail.com>
2023-04-09 14:56:41 +0300

    update and fix popup.js

--- js/content.js
+++ js/content.js
@@ -1,56 +1,36 @@
+// Намагаємось знайди в переданому тексті 't' суму та код валюти, інакше - NaN
 function StrToCurrency(t){
-    const match = t.match(/([\d.,\ ]+)\s*([^\d]{0,})/);
-    var currencyArr = {};
-    if (match) {
-        currencyArr.amount = parseFloat(match[1].replaceAll("", "").replaceAll(", ", ".").trim());
-        currencyArr.code = match[2].trim().toUpperCase();
-        return currencyArr;
-    } else{ return NaN; };
+  const match = t.match(/([\d.,\ ]+)\s*([^\d]{0,})/);
+  var currencyArr = {};
+  if (match) {
+      currencyArr.amount = parseFloat(match[1].replaceAll("", "").replaceAll(", ", ".").trim());
+      currencyArr.code = match[2].trim().toUpperCase();
+      return currencyArr;
+  } else{ return NaN; };
 }
 
-    // назначаем событие на отпускание мышки после выделения текста на странице
-  document.addEventListener('mouseup', function(){
-
-    var seltext = window.getSelection().toString();
-    if (seltext.length <= 20){
-      var currencyArr = StrToCurrency(seltext);
-      if (currencyArr.code){
-        $('#amount').val(currencyArr.amount);
-        // если нашли код валюты в выделенном тексте и этот код не 'UAH'
-        if (currencyArr.code && (currencyArr.code != 'UAH')){
-          // ассоциативный массив для всех возможных кодов валют
-          var assocArr = {};
-          // выбираем из json-объекта ключи 'cc', которые соответствуют коду валюты
-          promise_obj.then(data => {
-            var cc_values = data.map(element => element.cc);
-            cc_values.forEach(currency => {
-              // если данный код валюты присутствует в ключах my_cc_codes
-              if (my_cc_codes[currency]){
-                assocArr[currency] = [currency].concat(my_cc_codes[currency]);
-              }
-            });
-            // если код валюты выделенной строки присутствует в выпадающем списке, то его и выбрать
-            for (const key in assocArr){
-              if (assocArr[key].includes(currencyArr.code)){
-                // устанавливаем значение в выпадающем списке равным найденому коду валюты
-                $('#select-currency option[value="'+key+'"]').prop('selected', true);
-                break;
-              };
-            };
-            var rate = $('#select-currency option:selected').attr('data-id');
-            var convertedText = ($('#amount').val() * rate).toFixed(2) + ' UAH';
-            // Создаем элемент-контейнер, который будет заменять выделенный текст
-            var newElement = document.createElement("span");
-            newElement.style.color = "red"; // добавляем красный цвет тексту внутри span
-            newElement.style.backgroundColor = "yellow"; // добавляем желтый фон для span
-            newElement.appendChild( document.createTextNode(convertedText) );
-            // Заменяем выделенный текст на новый элемент с помощью Range и Selection API
-            var range = window.getSelection().getRangeAt(0);
-            range.deleteContents();
-            range.insertNode(newElement);
-          });
-        }
-      };
-    };
-
-  })
+// призначаємо подію на відпускання миші після виділення текста на сторінці
+document.addEventListener('mouseup', function(){
+var seltext = window.getSelection().toString();
+if (seltext.length <= 20){
+  chrome.storage.local.set({ selectedText: seltext });
+  var paraAmountCode = StrToCurrency(seltext);
+  if (paraAmountCode.amount && paraAmountCode.code && paraAmountCode.code != 'ГРН.'){
+    chrome.storage.local.get(['currencies'], function(data) {
+      const currencies = Object.values(data['currencies']);
+      // Якщо в currenciesArr є currencyCode, повернемо атрибут rate, якщо ні, повернемо 0
+      const rate = currencies.find(rate => rate.cc.includes(paraAmountCode.code))?.rate || 0;
+      var convertedText = (paraAmountCode.amount * rate).toFixed(2) + ' грн.';
+      // Создаем элемент-контейнер, который будет заменять выделенный текст
+      var newElement = document.createElement("span");
+      newElement.style.color = "red"; // добавляем красный цвет тексту внутри span
+      newElement.style.backgroundColor = "yellow"; // добавляем желтый фон для span
+      newElement.appendChild( document.createTextNode(convertedText) );
+      // Заменяем выделенный текст на новый элемент с помощью Range и Selection API
+      var range = window.getSelection().getRangeAt(0);
+      range.deleteContents();
+      range.insertNode(newElement);
+    });
+  }
+}
+});
